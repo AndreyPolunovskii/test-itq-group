276e813afb9c77855d8bdfce8d74d61cdc26ea54
AndreyPolunovskii <apap2009@yandex.ru>
2020-01-20 16:29:16 +0300

    добавили возможность сравнивать графики

--- itq_test_front/src/components/Draw_graphs.jsx
+++ itq_test_front/src/components/Draw_graphs.jsx
@@ -17,10 +17,14 @@ class DrawGraphs extends React.Component {
     this.state = {
       isFetching: true,
       error: null,
-      data : null,
-      start_date : "2020-01-13",
-      end_date : "2020-01-14",
-      city_name : "Moscow",
+      data1 : null,
+      data2 : null,
+      start_date1 : "2020-01-13",
+      end_date1 : "2020-01-14",
+      city_name1 : "Moscow",
+      start_date2 : "2020-01-13",
+      end_date2 : "2020-01-14",
+      city_name2 : "Paris",
       style : {
         height_graph : "400px"
       }
@@ -57,13 +61,27 @@ class DrawGraphs extends React.Component {
     }
   }
   ///////////////////////
-  PutState(event) {
+  PutState(event,type_data) {
 
-    axios.get(base_url+'/api/v1.0/data/upload',{
+    if (type_data === 'data1')
+    {
+      var my_city = this.state.city_name1;
+      var my_start_date = this.state.start_date1;
+      var my_end_date = this.state.end_date1;
+    }
+      else
+    {
+      var my_city = this.state.city_name2;
+      var my_start_date = this.state.start_date2;
+      var my_end_date = this.state.end_date2;
+    }
+
+
+    axios.get(base_url+'/data/upload',{
       params: {
-        city : this.state.city_name,
-        start_date : this.state.start_date,
-        end_date : this.state.end_date
+        city : my_city,
+        start_date : my_start_date,
+        end_date : my_end_date
       },
       headers: {
         // Перечисляем разрешённые методы
@@ -75,7 +93,7 @@ class DrawGraphs extends React.Component {
         this.checkData(response);
         console.log(response) // выводим в консоль получаемые обьекты
         this.setState({
-          data: response.data
+          [type_data]: response.data
         });
       }
     )
@@ -91,13 +109,27 @@ class DrawGraphs extends React.Component {
 }
 
 ///////////////////////
-UpdateState(event) {
+UpdateState(event,type_data) {
 
-  axios.get(base_url+'/api/v1.0/data/get',{
+  if (type_data === 'data1')
+  {
+    var my_city = this.state.city_name1;
+    var my_start_date = this.state.start_date1;
+    var my_end_date = this.state.end_date1;
+  }
+    else
+  {
+    var my_city = this.state.city_name2;
+    var my_start_date = this.state.start_date2;
+    var my_end_date = this.state.end_date2;
+  }
+
+
+  axios.get(base_url+'/data/get',{
     params: {
-      city : this.state.city_name,
-      start_date : this.state.start_date,
-      end_date : this.state.end_date
+      city : my_city,
+      start_date : my_start_date,
+      end_date : my_end_date
     },
     headers: {
       // Перечисляем разрешённые методы
@@ -109,7 +141,7 @@ UpdateState(event) {
       this.checkData(response);
       console.log(response) // выводим в консоль получаемые обьекты
       this.setState({
-        data: response.data
+        [type_data]: response.data
       });
     }
   )
@@ -132,10 +164,18 @@ handleClick(event) {
   event.preventDefault();
 
   if (name === "get")
-  this.UpdateState(event)
+  {
+    this.UpdateState(event,'data1')
+    this.UpdateState(event,'data2')
+  }
+
 
   if (name === "upload")
-  this.PutState(event)
+  {
+    this.PutState(event,'data1')
+    this.PutState(event,'data2')
+  }
+
 
 }
 //////////////////////////////////////
@@ -152,18 +192,23 @@ onChange(e) {
 //////////////////////
 componentDidMount(event) {
 
-  this.UpdateState(event)
+  this.UpdateState(event,'data1')
+  this.UpdateState(event,'data2')
 
 }
+
 /////////////////////
 //функция рисования графика
 getOption(type_data) {
+
+  var months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+                  "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
 
   let title = "",type_unit = "";
   if (type_data === "temp")
   {
     title = "Температура"
-    type_unit ="[C]"
+    type_unit ="[°C]"
   }
 
 
@@ -173,8 +218,6 @@ getOption(type_data) {
     type_unit ="[mb]"
   }
 
-
-
   if (type_data === "rh")
   {
     title = "влажность"
@@ -183,33 +226,67 @@ getOption(type_data) {
 
 
   //ждем пока данные полностью загрузятся и тогда отрисовываем график
-  if (this.state.data !== null)
+  if (this.state.data1 !== null && this.state.data2 !== null)
   {
 
     let option = {
       legend: {},
-      title: {text:type_unit},
-      tooltip: {},
-      dataset: {
-        dimensions: ['date',type_data],
-        source: this.state.data['data'].map(
-          (item) => (
-            {
-              date: new Date(item.dt),
-              [type_data]: item[type_data]
-            }
-          )
-        )
+      elements: {
+        point: {
+          display: true
+        }
       },
-      xAxis: {type: 'time'},
+      title: {text:type_unit},
+      tooltip: {
+        trigger: 'axis'
+        },
+      xAxis: {
+        axisTick: {
+          alignWithLabel: true
+        },
+        axisLine: {
+          onZero: false
+        },
+        axisLabel : {
+          formatter: function (value, index) {
+            // Formatted to be month/day; display year only in the first label
+            let myDate = new Date(value);
+            let fullDate = myDate.getDate() + " " + months[myDate.getMonth()] + " " + myDate.getFullYear() + " " + myDate.getHours() +":"+myDate.getMinutes()+":"+myDate.getSeconds() ;
+            return fullDate;
+          }
+        },
+        axisPointer: {
+          label: {
+            formatter: function (date) {
+              let myDate = new Date(date.value);
+              let fullDate = myDate.getDate() + " " + months[myDate.getMonth()] + " " + myDate.getFullYear() + " " + myDate.getHours() +":"+myDate.getMinutes()+":"+myDate.getSeconds() ;
+              return fullDate;
+            }
+          }
+        },
+        data: this.state.data1['data'].map((item) => (item.dt))
+      },
       yAxis: {},
       series: [
         {
-          name: title,
+          data: this.state.data1['data'].map(function (item) {
+            return item[type_data];
+          }),
+          name: this.state.data1['data'][0]['city_name'] +" (1)",
           type: 'line',
           smooth: 0.5  //включаем сглаживание
         },
+        {
+          data: this.state.data2['data'].map(function (item) {
+            return item[type_data];
+          }),
+          name: this.state.data2['data'][0]['city_name']+" (2)",
+          type: 'line',
+          smooth: 0.5  //включаем сглаживание
+        }
       ]
+
+
     };
     return option;
   }
@@ -222,9 +299,10 @@ getOption(type_data) {
 }
 ////////////////////
 renderTitle(data) {
-  if (this.state.data !== null)
+  if (this.state.data1 !== null && this.state.data2 !== null)
   {
-    return( <p><b>Выбранный город - {this.state.data['data'][0]['city_name']} ({this.state.data['data'][0]['country_code']})</b></p> );
+    return( <p><b>Выбранные города - {this.state.data1['data'][0]['city_name']} ({this.state.data1['data'][0]['country_code']})
+                       и {this.state.data2['data'][0]['city_name']} ({this.state.data2['data'][0]['country_code']})</b></p> );
   }
   else {
     return(<p></p>);
@@ -232,85 +310,112 @@ renderTitle(data) {
 
 }
 
+/////////////////////
+
 
 render() {
   let {style,data} = this.state;
-    return (
-      <div>
-        <div className={cs.page__wrapper}>
-          <div className={`${cs.page__title_wrapper} ${cs.page__main_title_wrapper}`}>
-            <b>Погодные данные</b>
-            <hr></hr>
-          </div>
-          <div className={cs.page__form_data_wrapper}>
-            <p className={cs.page__title_form_data_wrapper}><b>Введите основные параметры:</b></p>
-            <Form>
-              <Form.Row>
-                <Form.Group  as={Col} controlId="formHorizontal1">
-                  <div className={cs.row_three_item}>
-                    <Form.Label bsPrefix={cs.title_field}><b>Начальная дата:</b></Form.Label>
-                    <Form.Control name="start_date" defaultValue="2020-01-13" type="date" required="required"  onChange={this.onChange} />
-                  </div>
-                </Form.Group>
+  return (
+    <div>
+      <div className={cs.page__wrapper}>
+        <div className={`${cs.page__title_wrapper} ${cs.page__main_title_wrapper}`}>
+          <b>Погодные данные</b>
+          <hr></hr>
+        </div>
+        <div className={cs.page__form_data_wrapper}>
+          <p className={cs.page__title_form_data_wrapper}><b>Введите основные параметры:</b></p>
+          <hr></hr>
+          <p className={cs.page__title_form_one_data_wrapper}><b>Первый набор данных:</b></p>
+          <Form>
+            <Form.Row>
+              <Form.Group  as={Col} controlId="formHorizontal1">
+                <div className={cs.row_three_item}>
+                  <Form.Label bsPrefix={cs.title_field}><b>Начальная дата:</b></Form.Label>
+                  <Form.Control name="start_date1" defaultValue="2020-01-13" type="date" required="required"  onChange={this.onChange} />
+                </div>
+              </Form.Group>
 
-                <Form.Group as={Col} controlId="formHorizontal2">
-                  <div className={cs.row_three_item}>
-                    <Form.Label bsPrefix={cs.title_field}><b>Конечная дата:</b></Form.Label>
-                    <Form.Control name="end_date" defaultValue="2020-01-14" type="date" required="required" onChange={this.onChange} />
-                  </div>
-                </Form.Group>
+              <Form.Group as={Col} controlId="formHorizontal2">
+                <div className={cs.row_three_item}>
+                  <Form.Label bsPrefix={cs.title_field}><b>Конечная дата:</b></Form.Label>
+                  <Form.Control name="end_date1" defaultValue="2020-01-14" type="date" required="required" onChange={this.onChange} />
+                </div>
+              </Form.Group>
 
-                <Form.Group as={Col} controlId="formHorizontal2">
-                  <div className={cs.row_three_item}>
-                    <Form.Label bsPrefix={cs.title_field}><b>Название города (на английском):</b></Form.Label>
-                    <Form.Control name="city_name" defaultValue="Moscow" type="text" required="required" onChange={this.onChange} />
-                  </div>
-                </Form.Group>
-              </Form.Row>
-              <p><i>Внешний ресурс https://www.weatherbit.io/ ограничивает количество запрашивемых данных с разным интервалом времени до одного раза в сутки.</i>
-              <br></br><i>Соотвественно в сутки можно загрузить с внешнего ресурса только один интервал времени для любого города.</i>
-              <br></br><i>Максимальное количество запросов к внешнему ресурсу ограничивается до 500 в сутки.</i></p>
-              <Row>
-                <Col xs={4}>
-                    <Button type="submit" variant="warning" name="upload" className={cs.button_upload}  onClick={this.handleClick}>
-                      Загрузить данные с внешнего ресурса
-                    </Button>
-                </Col>
-                <Col xs={4}>
-                    <Button type="submit" variant="danger" name="get" className={cs.button_upload}  onClick={this.handleClick}>
-                      Загрузить данные из локальной базы
-                    </Button>
-                </Col>
-              </Row>
-            </Form>
-            <hr></hr>
-          </div>
+              <Form.Group as={Col} controlId="formHorizontal2">
+                <div className={cs.row_three_item}>
+                  <Form.Label bsPrefix={cs.title_field}><b>Название города (на английском):</b></Form.Label>
+                  <Form.Control name="city_name1" defaultValue="Moscow" type="text" required="required" onChange={this.onChange} />
+                </div>
+              </Form.Group>
+            </Form.Row>
+            <p className={cs.page__title_form_one_data_wrapper}><b>Второй набор данных:</b></p>
+            <Form.Row>
+              <Form.Group  as={Col} controlId="formHorizontal1">
+                <div className={cs.row_three_item}>
+                  <Form.Label bsPrefix={cs.title_field}><b>Начальная дата:</b></Form.Label>
+                  <Form.Control name="start_date2" defaultValue="2020-01-13" type="date" required="required"  onChange={this.onChange} />
+                </div>
+              </Form.Group>
 
-          <div className={cs.page__title_wrapper}>
-            {this.renderTitle(data)}
-          </div>
-          <div className={cs.page__graph_data_wrapper}>
+              <Form.Group as={Col} controlId="formHorizontal2">
+                <div className={cs.row_three_item}>
+                  <Form.Label bsPrefix={cs.title_field}><b>Конечная дата:</b></Form.Label>
+                  <Form.Control name="end_date2" defaultValue="2020-01-14" type="date" required="required" onChange={this.onChange} />
+                </div>
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formHorizontal2">
+                <div className={cs.row_three_item}>
+                  <Form.Label bsPrefix={cs.title_field}><b>Название города (на английском):</b></Form.Label>
+                  <Form.Control name="city_name2" defaultValue="Paris" type="text" required="required" onChange={this.onChange} />
+                </div>
+              </Form.Group>
+            </Form.Row>
+            <p><i>Внешний ресурс https://www.weatherbit.io/ ограничивает количество запрашивемых данных с разным интервалом времени до одного раза в сутки.</i>
+            <br></br><i>Соотвественно в сутки можно загрузить с внешнего ресурса только один интервал времени для любого города.</i>
+            <br></br><i>Максимальное количество запросов к внешнему ресурсу ограничивается до 500 в сутки.</i></p>
             <Row>
-              <Col>
-                <ReactEcharts ref='echartsInstance'
-                  style={{ height: style.height_graph}}
-                  option={this.getOption('temp')} />
+              <Col xs={4}>
+                <Button type="submit" variant="warning" name="upload" className={cs.button_upload}  onClick={this.handleClick}>
+                  Загрузить данные с внешнего ресурса
+                </Button>
               </Col>
-              <Col>
-                <ReactEcharts ref='echartsInstance'
-                  style={{ height: style.height_graph}}
-                  option={this.getOption('pres')} />
-              </Col>
-              <Col>
-                <ReactEcharts ref='echartsInstance'
-                  style={{ height: style.height_graph}}
-                  option={this.getOption('rh')} />
+              <Col xs={4}>
+                <Button type="submit" variant="danger" name="get" className={cs.button_upload}  onClick={this.handleClick}>
+                  Загрузить данные из локальной базы
+                </Button>
               </Col>
             </Row>
-          </div>
+          </Form>
+          <hr></hr>
+        </div>
+
+        <div className={cs.page__title_wrapper}>
+          {this.renderTitle(data)}
+        </div>
+        <div className={cs.page__graph_data_wrapper}>
+          <Row>
+            <Col>
+              <ReactEcharts ref='echartsInstance'
+                style={{ height: style.height_graph}}
+                option={this.getOption('temp')} />
+            </Col>
+            <Col>
+              <ReactEcharts ref='echartsInstance'
+                style={{ height: style.height_graph}}
+                option={this.getOption('pres')} />
+            </Col>
+            <Col>
+              <ReactEcharts ref='echartsInstance'
+                style={{ height: style.height_graph}}
+                option={this.getOption('rh')} />
+            </Col>
+          </Row>
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 }
